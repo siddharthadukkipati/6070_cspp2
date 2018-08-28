fc5d4a09bfbdb9c5559f00b0687165fac058f1f1
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-08-28 18:26:48 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2-assignment/M2/Assignment-3/Solution.java
+++ CSPP2-assignment/M2/Assignment-3/Solution.java
@@ -7,32 +7,34 @@ import java.util.Scanner;
 *the recursive function.
 */
 public final class Solution {
-	/**
-	*Do not modify this main function.
-	*/
-	private Solution() {
-	/**
-	*this is a constructor.
+    /**
+    *Do not modify this main function.
     */
-	}
-	/**
-	*declaring the main function.
-	*where we call the power function.
-	*/
-	public static void main(final String[] args) {
-		Scanner s = new Scanner(System.in);      
-        long base = s.nextInt();
-        long exponent = s.nextInt();
-        long result = power(base, exponent);
+    private Solution() {
+    /**
+    *this is a constructor.
+    */
+    }
+    /**
+    *declaring the main function.
+    *where we call the power function.
+    */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in); 
+        final long base = s.nextInt();
+        final long exponent = s.nextInt();
+        final long result = power(base, exponent);
         System.out.println(result);
-	}
-	/**
-	*Need to write the power function and print the output.
-	*/
-	public static long power(long base, long exponent) {
-		if (exponent == 0)
-			return 1;
-		else
-			return base * power(base, exponent-1);
-	}
+    }
+    /**
+    *Need to write the power function and print the output.
+    */
+    public static long power(final long base, final long exponent) {
+        if (exponent == 0) {
+            return 1;
+        }
+        else {
+            return base * power(base, exponent - 1);
+        }
+    }
 }
