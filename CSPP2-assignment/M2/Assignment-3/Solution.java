32de749e79f3b6ed8f2e2405b6e448bb81cac9bb
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-08-28 18:54:36 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2-assignment/M2/Assignment-3/Solution.java
+++ CSPP2-assignment/M2/Assignment-3/Solution.java
@@ -28,12 +28,12 @@ public final class Solution {
     *Need to write the power function and print the output.
     *@param base long.
     *@param exponent long.
+    *@return power value.
     */
     public static long power(final long base, final long exponent) {
         if (exponent == 0) {
             return 1;
-        }
-        else {
+        } else {
             return base * power(base, exponent - 1);
         }
     }
