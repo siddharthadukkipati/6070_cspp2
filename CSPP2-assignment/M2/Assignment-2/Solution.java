05db406a5c9771da0146b89c72c2e1341e08fd4a
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-08-28 18:31:29 +0530

    "check_style_score -> 3 of 4 passed. style score: 0/1 "

---
+++ CSPP2-assignment/M2/Assignment-2/Solution.java
@@ -0,0 +1,40 @@
+/*
+importing the scanner.
+*/
+import java.util.Scanner;
+// import static java.lang.Math.sqrt;
+/**
+*the program is to find the roots.
+*/
+public final class Solution {
+    /**
+    *the program is to find the roots.
+    */
+    private Solution() {
+    }
+    /**
+    *Do not modify this main function.
+    */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        roots(a, b, c);
+        // System.out.println(root2);
+    }
+    /*function to find out the roots of the Quadratic Equation.
+    */
+    public static void roots(final int a, final int b, final int c) {
+        double result1, result2;
+        if ((b * b) - (2 * 2 * a * c) >= 0) {
+        result1 = (-b) + Math.sqrt((b * b) - (2 * 2 * a * c));
+        result2 = (-b) - Math.sqrt((b * b) - (2 * 2 * a * c));
+        System.out.println(result1 / 2 * a + " " + result2 / 2 * a);
+        }
+    }
+    /*
+    Need to write the rootsOfQuadraticEquation function.
+    and print the output.
+    */
+}
