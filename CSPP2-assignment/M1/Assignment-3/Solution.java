299244fc80e80d76cb78e135f214877f72c5e1e1
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-08-28 10:05:55 +0530

    Solution.java

---
+++ CSPP2-assignment/M1/Assignment-3/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+	/*
+	Do not modify this main function.
+	*/
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		double celsius = scan.nextDouble();
+		double fahrenheit = (celsius * 9 / 5) + 32;;
+		System.out.println(fahrenheit);
+	}
+	/*
+	Need to fill the calculateFromDegreesToFarenheit function and print the output
+	of fahrenheit.
+	*/
+}
