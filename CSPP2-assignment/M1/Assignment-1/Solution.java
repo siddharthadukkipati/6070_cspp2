902b90e541015d65105092628d86f65d89d941c1
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-08-28 09:57:30 +0530

    Solution.java

---
+++ CSPP2-assignment/M1/Assignment-1/Solution.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Solution {
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		String name = sc. nextLine();
+		int rollnumber = sc.nextInt();
+		System.out.println(name +","+rollnumber);
+	}
+}
