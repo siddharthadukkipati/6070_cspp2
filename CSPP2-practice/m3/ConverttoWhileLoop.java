2a6ea22d9b1b9fc13964024b9b0dc7afc55c70f3
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-08-29 10:25:49 +0530

    ConverttoWhileLoop

---
+++ CSPP2-practice/m3/ConverttoWhileLoop.java
@@ -0,0 +1,24 @@
+/**
+*This is the main class in which we are trying to get the.
+*while loop implemented.
+*/
+class ConverttoWhileLoop {
+    /**
+    *This is a default constructor.
+    */
+    private Convert {
+
+    }
+/**
+*We are declaring the main method in which we write the code.
+*@param args String
+*/
+    public static void main(String[] args) {
+        final int i = 2;
+        while (i <= 10) {
+            System.out.println(i);
+            i += 2;
+        }
+    System.out.println("GoodBye!");
+    }
+}
