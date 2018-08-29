ecabb0870245bd2a59d4420b118f238e31b86011
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-08-29 15:02:43 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2-assignment/M3/Assignment-3/Solution.java
+++ CSPP2-assignment/M3/Assignment-3/Solution.java
@@ -12,10 +12,10 @@ public final class Solution {
     private Solution() {
     }
     /**
-    *defining main method
+    *defining main method.
     *@param args String.
     */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
 
         Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
