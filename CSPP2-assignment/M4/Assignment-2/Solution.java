426fd486032bcd6447d51dc1946aa2c7d6d4ac1c
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-08-30 15:22:37 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- CSPP2-assignment/M4/Assignment-2/Solution.java
+++ CSPP2-assignment/M4/Assignment-2/Solution.java
@@ -17,31 +17,31 @@ public class Solution {
     */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int row1 = scan.nextInt();
-        int col1 = scan.nextInt();
-        int[][] matrix1 = new int[row1][col1];
-        int[][] result = new int[row1][col1];
-        for (int i = 0; i < row1; i++) {
-            for (int j = 0; j < col1; j++) {
-                matrix1[i][j] = scan.nextInt();
+        int r1 = scan.nextInt();
+        int c1 = scan.nextInt();
+        int[][] m1 = new int[r1][c1];
+        int[][] r = new int[r1][c1];
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c1; j++) {
+                m1[i][j] = scan.nextInt();
             }
         }
-        int row2 = scan.nextInt();
-        int col2 = scan.nextInt();
-        int[][] matrix2 = new int[row2][col2];
-        for (int i = 0; i < row2; i++) {
-            for (int j = 0; j < col2; j++) {
-                matrix2[i][j] = scan.nextInt();
+        int r2 = scan.nextInt();
+        int c2 = scan.nextInt();
+        int[][] m2 = new int[r2][c2];
+        for (int i = 0; i < r2; i++) {
+            for (int j = 0; j < c2; j++) {
+                m2[i][j] = scan.nextInt();
             }
         }
-        if (row1 == row2 && col1 == col2) {
-            for (int i = 0; i < row1; i++) {
-                for (int j = 0; j < col1 - 1; j++) {
-                    result[i][j] = matrix1[i][j] + matrix2[i][j];
-                    System.out.print(result[i][j] + " ");
+        if (r1 == r2 && c1 == c2) {
+            for (int i = 0; i < r1; i++) {
+                for (int j = 0; j < c1 - 1; j++) {
+                    r[i][j] = m1[i][j] + m2[i][j];
+                    System.out.print(r[i][j] + " ");
                 }
-                result[i][col1 - 1] = matrix1[i][col1 - 1] + matrix2[i][col2 - 1];
-                System.out.println(result[i][col1 - 1]);
+                r[i][c1 - 1] = m1[i][c1 - 1] + m2[i][c2 - 1];
+                System.out.println(r[i][c1 - 1]);
             }
         } else {
             System.out.println("not possible");
