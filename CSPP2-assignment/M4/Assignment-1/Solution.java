e8eba9a3592ccc42215b9a421c687b6df5f5fcdb
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-08-30 14:20:22 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

---
+++ CSPP2-assignment/M4/Assignment-1/Solution.java
@@ -0,0 +1,40 @@
+/**
+*importing Scanner from java.util.
+*/
+import java.util.Scanner;
+/**
+*defining the class Solution.
+*/
+public class Solution {/*
+    Fill this main function to print maximum of given array
+    */
+    /**
+    *defining the constructor.
+    */
+    private void solution() {
+    }
+    /**
+    *defining the main method
+    *@param args String.
+    */
+    public static void main(final String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        // System.out.println("Enter the array size");
+        int n= sc.nextInt();
+        int[] a = new int[n];
+        int max;
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        max = a[0];
+        for (int i = 0; i < n; i++) {
+            if (max < a[i]) {
+                max = a[i];
+            }
+        }
+        System.out.println(max);
+        
+
+    }
+}
